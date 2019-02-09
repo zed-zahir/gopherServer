e8f8177207194f7807698bd6a2aa2990a0cdbdbc
zed-zahir <zahir.meddour@protonmail.com>
2019-02-09 03:49:45 +0100

    I don't know what I am doing with the server

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import socket
 import os
 import sys
+import time
 
 print( """
         script as sgopher server
@@ -14,19 +15,21 @@ s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 s.bind( ( sys.argv[ 1 ], int( sys.argv[ 2 ] ) ) )
 s.listen( 1 )
 connection, client = s.accept()
+time.sleep( 1 )
 connection.send( "=== Welcome to the Gopher server written in python by zahir meddour ===\n".encode())
 while 1:
     request = connection.recv( 1024 )
+    if request == None:
+        continue
     dataRequest = request.split()
     print( client, dataRequest )
     item = dataRequest[ 0 ]
-    print( item )
     if item.decode() == "0":
         try:
             fileToOpen = dataRequest[1]
         except IndexError:
             connection.send( "you didn't specify a file to open\n".encode() )
-            print( "the peer didn't specify a file to open" )
+            print( "[0] the peer didn't specify a file to open" )
             continue
         fileToOpen = dataRequest[ 1 ]
         try:
@@ -54,7 +57,19 @@ while 1:
         connection.send( "this feature is not supported yet\n".encode() )
     elif item.decode() == "5":
         #TODO Support this feature
+
+        try:
+            fileToOpen = dataRequest[1]
+        except IndexError:
+            connection.send( "you didn't specify a file to download\n".encode() )
+            print( "[5] the peer didn't specify a file to download" )
+            continue
+
+        fRead = freadlines()
+        print( fRead[ 0:2 ] )
+        
         connection.send( "this feature is not supported yet\n".encode() )
+    
     elif item.decode() == "6":
         #TODO Support this feature
         connection.send( "this feature is not supported yet\n".encode() )
@@ -67,9 +82,13 @@ while 1:
     elif item.decode() == "9":
         #TODO Support this feature
         connection.send( "this feature is not supported yet\n".encode() )
+    elif item.decode() == None:
+        connection.send( "you didn't specify a parameter try 0 <file>\n".encode() )
+        print( "there is not parameter specified" )
+        connection.close()
     else:
-        connection.send( "the protocol is broken, try 0 <file>\n".encode() )
-        print( "the protocol is broken" )
-
+        connection.send( "the protocol is brokden, try 0 <file>\n".encode() )
+        print( "wrong parameter has been specified" )
+        connection.close()
 s.close()
 
