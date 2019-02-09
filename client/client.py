c941a3c8a27a830d28957605c3a2b52a7eea9700
zed-zahir <zahir.meddour@protonmail.com>
2019-02-09 09:19:31 +0100

    add bash command parameters condition

--- client/client.py
+++ client/client.py
@@ -6,28 +6,31 @@ import sys
 import time
 
 
-s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-s.connect( ( sys.argv[ 1 ], int( sys.argv[2] ) ) )
-print( s.recv( 1024 ).decode() )
-if sys.argv[ 3 ] == "text":
-    message = "0 " + sys.argv[ 4 ]
-    s.send( message.encode() )
-    print(s.recv( 1024 ).decode())
-    s.close()
-elif sys.argv[ 3 ] == "dos":
-    message = "5 " + sys.argv[ 4 ]
-    s.send( message.encode() )
-    receivedMessage =  s.recv( 1024 ).decode()
-    print( receivedMessage )
-    if "the file exist" in receivedMessage:
-        answerDownload = input(  )
-        if "yes" in answerDownload:
-            s.send( "yes".encode() )
-            f = open( sys.argv[ 4 ], "wb" )
-            f.write( s.recv( 5000 ) )
-            f.close()
-        else:
-            s.send( "no".encode() )
-    s.close()
+if len( sys.argv ) != 5:
+    print( "usage: script <ip address> <tcp port> <command> <the file>" )
 else:
-    os.exit()
+    s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+    s.connect( ( sys.argv[ 1 ], int( sys.argv[2] ) ) )
+    print( s.recv( 1024 ).decode() )
+    if sys.argv[ 3 ] == "text":
+        message = "0 " + sys.argv[ 4 ]
+        s.send( message.encode() )
+        print(s.recv( 1024 ).decode())
+        s.close()
+    elif sys.argv[ 3 ] == "dos":
+        message = "5 " + sys.argv[ 4 ]
+        s.send( message.encode() )
+        receivedMessage =  s.recv( 1024 ).decode()
+        print( receivedMessage )
+        if "the file exist" in receivedMessage:
+            answerDownload = input(  )
+            if "yes" in answerDownload:
+                s.send( "yes".encode() )
+                f = open( sys.argv[ 4 ], "wb" )
+                f.write( s.recv( 5000 ) )
+                f.close()
+            else:
+                s.send( "no".encode() )
+        s.close()
+    else:
+        os.exit()
