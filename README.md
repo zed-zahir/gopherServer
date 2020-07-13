6246ccaa54d49fdd40b2940bca9eae38829accfb
zed-zahir <zahir.meddour@protonmail.com>
2020-07-13 14:02:37 +0100

    comeback to the old code

--- README.md
+++ README.md
@@ -1,16 +1,37 @@
-# A gopher client script in python
+# gopherServer script in python
 
-It's a gopher client in pyhton.
-Run it as.
+how to use this script in sudo based Unix like OS
 ```bash
-./client.py gopher://gopher.floodgap.com/1/world
+$ cd server/
+$ sudo ./server.py 127.0.0.1 70
 ```
-### for now it doesn't print the whole page, and I don't know why!!
+or if you don't want to use the protocol in reserved tcp port, meaning without super user privileges
+```bash
+$ cd server/
+$ ./server.py 127.0.0.1 7000
+```
+in another terminal use
+```bash
+$ ./client/client.py 127.0.0.1 70 text PATH/sample.txt
+```
+
+if the file exist then it will print on the terminal
+
+## or
+```bash
+$ ./client/client.py 127.0.0.1 70 dos Bomb.ex_
+```
+if the file exist then there would be a prompt to accept or no
+
+## or
+```bash
+$ ./client/client.py 127.0.0.1 70 binary start
+```
 
 Gopher RFC is: rfc1436
 
 link: [rfc1436](https://www.ietf.org/rfc/rfc1436.txt)
 
-Here is the wikipedia article about the protocol.
+here is the wikipedia article about the protocol
 
 link [wikipedia Gopher protocol](https://en.wikipedia.org/wiki/Gopher_(protocol))
---
+++ client/client.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+
+import socket
+import os
+import sys
+import time
+from binascii import unhexlify
+
+# check the parameters
+
+if len( sys.argv ) != 3:
+    print( "usage: script <ip address> <tcp port>" )
+    sys.exit()
+
+# create the socket and connect
+
+else: 
+    s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+    s.connect( ( sys.argv[ 1 ], int( sys.argv[2] ) ) )
+
+    # print the text file read from the network steam
+
+    print( s.recv( 1024 ).decode() )
+
+    request = input()
+    if int(request) > 3 and int(request) < 1:
+        print( "there is an error while processing your request" )
+        sys.exit()
+
+    # send the parameter of 0 means "text"
+    
+    if int(request) == 1:
+        data = input( "the text file name?: " )
+        message = "0 " + data
+        s.send( message.encode() )
+        print(s.recv( 1024 ).decode())
+        s.close()
+
+    # send the parameter 5 emans "dos"
+
+    elif int(request) == 2:
+        data = input( "the dos filename?: " )
+        message = "5 " + data
+        s.send( message.encode() )
+        receivedMessage =  s.recv( 1024 ).decode()
+        print( receivedMessage )
+        
+        # check the condition if the user want to download the file
+        
+        if "the file exist" in receivedMessage:
+            answerDownload = input(  )
+            if "yes" in answerDownload:
+                s.send( "yes".encode() )
+
+                # open an empty file writable
+
+                f = open( data, "wb" )
+                print( "writing..." )
+
+                # recv the data decode it unhexfy it then write it to the f socket
+
+                f.write( unhexlify(s.recv( 5000 ).decode()) )
+                print( "done!" )
+                f.close()
+            else:
+                s.send( "no".encode() )
+        s.close()
+
+    # send the parameter 9 means "binary"
+
+    elif int(request) == 3:
+        data = input( "the binary filename?: " )
+        message = "9 " + data
+        s.send( message.encode() )
+        receivedMessage = s.recv( 1024 ).decode()
+        if "the file exist" in receivedMessage:
+            print( "the file " + sys.argv[ 4 ] + " exist" )
+
+            # open the file in binary and writable mode
+
+            f = open( data, "wb" )
+            print( "writing..." )
+
+            # recv and decode and unhexify then write the data to the f socket
+
+            f.write( unhexlify( s.recv( 5000 ).decode() ) )
+            print( "done!" )
+            f.close()
+        else:
+            s.close()
+    else:
+        print( "no parameter are specified" )
